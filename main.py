b39a2d2c8219a5d3854e27abe03fabfc8d172d9d
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-05-31 08:53:48 +0200

    Add 3 ufo image

--- main.py
+++ main.py
@@ -8,4 +8,13 @@ window = Tk()
 canvas = Canvas(window, width=WIDTH, height=HEIGHT)
 canvas.pack()
 
+ufo_image1 = PhotoImage(file='ufo.png')
+my_image1 = canvas.create_image(0, 0, image=ufo_image1, anchor=NW)
+
+ufo_image2 = PhotoImage(file='ufo.png')
+my_image2 = canvas.create_image(300, 200, image=ufo_image2, anchor=NW)
+
+ufo_image3 = PhotoImage(file='ufo.png')
+my_image3 = canvas.create_image(800, 100, image=ufo_image3, anchor=NW)
+
 window.mainloop()
